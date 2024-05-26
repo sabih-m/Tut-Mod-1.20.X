aa1dabf5c253e22a299d4bddcfd16e9a80494b42
sabih <sabihnoidea@gmail.com>
2024-05-26 09:51:45 +0500

    Initial commit

---
+++ src/main/java/net/sabih/tutmod/TutModClient.java
@@ -0,0 +1,10 @@
+package net.sabih.tutmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
